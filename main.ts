74ea9b9f35b8278040875078aed1292376862627
Dyanna Azevedo <dyannavasconcelosazevedo@gmail.com>
2021-11-09 22:45:05 -0300

    fixing main file

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read=./forca-em-ts --allow-write=./forca-em-ts
+#!/usr/bin/env -S deno run --allow-read=./ --allow-write=./
 
 import * as play from './play.ts';
 
